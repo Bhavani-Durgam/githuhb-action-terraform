fe2ffd496d8639d27a7218a63a5363b1be01bd73
Bhavani-Durgam <durgambhavani799@gmail.com>
2026-04-06 16:28:39 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ resource "aws_subnet" "name2" {
   
 }
 resource "aws_instance" "example_server" {
-  ami           = "ami-0c55b159cbfafe1f0" # Update with a valid AMI ID for your region
+  ami           = "ami-01b14b7ad41e17ba4" # Update with a valid AMI ID for your region
   instance_type = "t2.micro"
 
   tags = {
